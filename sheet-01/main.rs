57b7665bd526903c776bd6f48720ad45d408bdee
Guglielmo Boi <boi.guglielmo@gmail.com>
2023-10-05 20:53:54 +0200

    2023/10/05

---
+++ sheet-01/main.rs
@@ -0,0 +1,158 @@
+use std::collections::HashMap;
+
+/* 
+    1. Write a function string_reverse that takes a &str as input and returns it, reversed as a String.
+*/
+fn string_reverse(s: &str) -> String {
+    s.chars().rev().collect()
+}
+
+
+/*
+    2. Write a function bigger that takes two i32 and returns the bigger number (i32) without using
+    another function call and additional variables.
+*/
+fn bigger(a: i32, b: i32) -> i32 {
+    if a > b {
+        a
+    } else {
+        b
+    }
+}
+
+
+/*
+    3. Write a function multiply that takes an i32, a f32 and a f64 and returns the multiplication of the
+    three of them as a f64 value.
+*/
+fn multiply(a: i32, b: f32, c: f64) -> f64{
+    (a as f64) * (b as f64) * c
+}
+
+/*
+    4. Write a function e_equals_mc_squared that takes as input a f32 representing the mass, and that
+    uses a globally-defined constant containing the value of the speed of light in a vacuum (expressed in
+    m/s). The function outputs the energy equivalent to the mass input.
+*/
+const SPEED_OF_LIGHT: f32 = 299792458.0;
+
+fn e_equals_mc_squared(mass: f32) -> f32{
+    mass * SPEED_OF_LIGHT.powf(2.0)
+}
+
+
+/*
+    5. Given a vector of i32, create a function max_min that returns the maximum and the minimum value
+    inside that vector.
+*/
+fn max_min(vector: Vec<i32>) -> (i32, i32) {
+    (*(vector.iter().max().unwrap()), *(vector.iter().min().unwrap()))
+}
+
+
+/*
+    6. Write a function lord_farquaad that takes a String and outputs another String in which every
+    character 'e' is substituted by the character '💥'.
+*/
+fn lord_farquaad(s: String) -> String {
+    s.replace("e", "💥")
+}
+
+
+/*
+    7. In the main function initialize a HashMap<String, f32> called furniture that stores the pair
+    String as key and f32 as value, where the String is the name of the furniture and the f32 is its
+    price. Then write a function that borrows the HashMap, takes a furniture: String as input and
+    returns the corresponding f32. If there is no such furniture in the HashMap, return -1.0.
+*/
+fn get_furniture_price(furniture_map: &HashMap<String, f32>, furniture: String) -> f32 {
+    if furniture_map.contains_key(&furniture) {
+        *(furniture_map.get(&furniture).unwrap())
+    } else {
+        -1.0
+    }
+}
+
+
+/*
+    8. Write a function append that takes a String, appends the word "foobar" to it and returns it.
+    Then write a main function in which you.
+    - Declare a String initialized with some text.
+    - Pass the String to the function append.
+    - Print the original String and the one returned by append.
+*/
+fn append_foobar(s: &String) -> String {
+    s.to_owned() + &String::from("foobar")
+}
+
+fn append() {
+    let s = String::from("Guglielmo");
+    let s_foobar = append_foobar(&s);
+    println!("{}", s);
+    println!("{}", s_foobar);
+}
+
+
+/*
+    9. An Armstrong number is a number that is the sum of its own digits each raised to the power of the
+    number of digits.
+    Write the function is_armstrong that determines whether a number is an Armstrong number.
+*/
+fn count_digits(num: i64) -> i64 {
+    if num == 0 {
+        0
+    } else {
+        count_digits(num / 10) + 1
+    }
+}
+
+fn is_armstrong(num: i64) -> bool {
+    let mut n = num;
+    let d = count_digits(num);
+    let mut sum = 0;
+
+    while n > 0 {
+        sum += (n % 10).pow(d as u32);
+        n /= 10;
+    }
+
+    num == sum
+}
+
+
+/*
+    10. Write a function that takes a 2x2 i32 "matrix" (2x2 tuple) as input, transposes and returns it.
+*/
+fn transpose(matrix: ((i32, i32), (i32, i32))) -> ((i32, i32), (i32, i32)) {
+    let ((a, b), (c, d)) = matrix;
+    ((a, c), (b, d))
+}
+
+
+fn main()
+{
+    println!("{}", string_reverse("Guglielmo"));
+
+    println!("{}", bigger(2, 5));
+    
+    println!("{}", multiply(2, 5.5, 10.7));
+    
+    println!("{}", e_equals_mc_squared(62.0));
+    
+    println!("{:?}", max_min(vec![0, 1, 2, 3, 4]));
+    
+    println!("{}", lord_farquaad(String::from("mele")));
+
+    let mut furniture_map = HashMap::new();
+    furniture_map.insert(String::from("chair"), 10.0);
+    furniture_map.insert(String::from("table"), 18.0);
+    furniture_map.insert(String::from("wardrobe"), 25.0);
+
+    println!("{}", get_furniture_price(&furniture_map, String::from("chair")));
+
+    append();
+
+    println!("{}", is_armstrong(9));
+
+    println!("{:?}", transpose(((0, 1), (2, 3))));
+}
---
+++ sheet-02/main.rs
@@ -0,0 +1,224 @@
+use std::collections::HashMap;
+
+/*
+    1. Write a function called modify_odd that takes a mutable reference to an vecay slice of
+    integers slice and sets all odd numbers to 0.
+    Then write a second function that create a Vec, filled with all numbers from 0 to 100,
+    and pass it to modify_odd.
+*/
+fn modify_odd(slice: &mut [i32]) {
+    for num in slice.iter_mut() {
+        if *num % 2 == 1 {
+            *num = 0;
+        }
+    }
+}
+
+
+/*
+    2. Write a function count_character that takes a string consisting of ASCII characters
+    string as input and returns a HashMap. The keys of the HashMap should be the
+    characters in the string, and the values should be an u32 representing how many times
+    each character appears in the string.
+*/
+fn count_character(str: String) -> HashMap<char, u32> {
+    let mut ret = HashMap::new();
+
+    for c in str.chars() {
+        ret.insert(c, 1 + if ret.contains_key(&c) { ret[&c] } else { 0 });
+    }
+
+    ret
+}
+
+
+/*
+    3. Write a function named split_at_value that takes two arguments: a slice of i32 called
+    slice and a single i32 value called value. The function should find the first element
+    equal to value inside slice. It should then split the slice at the corresponding index
+    and return the two resulting slices wrapped in an Option.
+    If value is not found in slice, the function should return None.
+*/
+fn split_at_value(slice: &[i32], value: i32) -> Option<(&[i32], &[i32])> {
+    if let Some(index) = slice.iter().position(|&x| x == value) {
+        Some(slice.split_at(index + 1))
+    } else {
+        None
+    }
+}
+
+
+/*
+    4. Write a function sub_slice that takes two &Vec<i32> as input. If the second vector is
+    contained inside the first one it print the corresponding slice, otherwise it print Not
+    found.
+*/
+fn sub_slice(first: &Vec<i32>, second: &Vec<i32>) {
+    if let Some(start_index) = first.iter().position(|&x| x == second[0]) {
+        if first[start_index..].starts_with(second) {
+            println!("{:?}", &first[start_index..start_index + second.len()]);
+            return
+        }
+    }
+
+    println!("Not found");
+}
+
+
+/*
+    5. Write the following functions, for each of the functions think carefully about what is the
+    best way to pass the arguments (&, &mut or passing ownership):
+    Write a function max that takes a Vec of i32 and returns the maximum value inside
+    it.
+    Write a function swap that swaps the first and last element of a vector of i32.
+    Write a function is_sorted that takes a Vec of i32 and returns a boolean indicating
+    whether the vector is sorted in non-decreasing order.
+    Write a function insert_if_longer that takes a Vec of String (vec) and a String
+    (string). This function should insert string into vec only if the length of string
+    is greater than 10.
+*/
+fn max(vec: &Vec<i32>) -> i32 {
+    *vec.iter().max().unwrap()
+}
+
+fn swap(vec: &mut Vec<i32>) {
+    let len = vec.len();
+
+    if len >= 2 {
+        vec.swap(0, len - 1);
+    }
+}
+
+fn is_sorted(vec: &Vec<i32>) -> bool {
+    let mut ret = true;
+
+    for i in 1..vec.len() {
+        if vec[i] < vec[i - 1] {
+            ret = false
+        }
+    }
+
+    ret
+}
+
+fn insert_if_longer(vec: &mut Vec<String>, str: String) {
+    if str.len() > 10 {
+        vec.push(str);
+    }
+}
+
+
+/*
+    6. Write a function build_vector that takes a Iter<i32> and returns the Vec<i32>
+    containing all the elements of the iterator.
+*/
+// fn build_vector(iter: Iter<i32>) -> Vec<i32> {
+//     iter.collect();
+// }
+
+
+/*
+    7. Write a function pancake_sort that takes a &mut Vec<i32> and sorts it using the
+    pancake sort algorithm.
+*/
+fn find_max_index(slice: &[i32]) -> i32 {
+    let mut max_index = 0;
+    let mut max_value = slice[0];
+    
+    for (i, &value) in slice.iter().enumerate() {
+        if value > max_value {
+            max_value = value;
+            max_index = i;
+        }
+    }
+    
+    max_index as i32
+}
+
+fn flip(vec: &mut Vec<i32>, end: usize) {
+    let mut start = 0;
+    let mut end = end;
+
+    while start < end {
+        vec.swap(start, end);
+        start += 1;
+        end -= 1;
+    }
+}
+
+fn pancake_sort(vec: &mut Vec<i32>) {
+    let len = vec.len() as i32;
+    let mut current_size = len;
+
+    while current_size > 1 {
+        let max_index = find_max_index(&vec[0..current_size as usize]);
+
+        if max_index != current_size - 1 {
+            flip(vec, max_index as usize);
+            flip(vec, (current_size - 1) as usize);
+        }
+
+        current_size -= 1;
+    }
+}
+
+
+/*
+    8. Write a function merge that takes two &[i32] and returns a Vec<i32>. The returned
+    vector should contain the elements of the two passed elements sorted, you can assume
+    that the passed slices are sorted.
+*/
+fn merge(a: &[i32], b: &[i32]) -> Vec<i32> {
+    let mut ret = Vec::new();
+    
+    ret.extend(a);
+    ret.extend(b);
+    ret.sort();
+    
+    ret
+}
+
+
+/*
+    9. Create a Vec that can contain both an i32 and a String.
+*/
+enum Foo
+{
+    Num(i32),
+    Str(String)
+}
+
+
+/*
+    
+*/
+
+fn main()
+{ 
+    let mut slice = [0, 1, 2, 3, 4];
+    modify_odd(&mut slice);
+    println!("{:?}", slice);
+
+    let cc = count_character(String::from("apple"));
+    println!("{:?}", cc);
+
+    println!("{:?}", split_at_value(&[0, 1, 2, 3, 4], 2).unwrap().0);
+
+    sub_slice(&vec![0, 1, 2, 3, 4], &vec![2, 3, 4]);
+
+    max(&vec![0, 1, 2, 3, 4]);
+    swap(&mut vec![0, 1, 2, 3, 4]);
+    println!("{}", is_sorted(&vec![0, 1, 2, 3, 4]));
+
+    insert_if_longer(&mut vec![String::from("str")], String::from("reallylongstring"));
+
+    pancake_sort(&mut vec![0, 2, 3, 1, 4]);
+
+    merge(&[0, 1, 2, 3, 4], &[0, 1, 2, 3, 4]);
+
+    let mut vec = Vec::new();
+    vec.push(Foo::Num(10));
+    vec.push(Foo::Str(String::from("apple")));
+
+    
+}
